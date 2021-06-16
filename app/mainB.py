d799035206f22d89b8ef2ac029d547b70d8e460d
XaviRubio_Client <xavirubio09@gmail.com>
2021-06-16 06:14:46 -0400

    Add bakery frontend and all the functions with no trained ai

---
+++ app/mainB.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from typing import Optional
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+import uvicorn
+import numpy as np
+import json
+
+from aibot import mainchatbot
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="../bakery-master"), name="static")
+templates = Jinja2Templates(directory="../bakery-master")
+
+class Data(BaseModel):
+    chatInput: str
+
+@app.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
+
+@app.post('/chat')
+def startchat(data: Data):
+    dict_chat = mainchatbot.chat(data.chatInput)
+    return dict_chat
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
